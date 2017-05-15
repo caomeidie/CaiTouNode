f04826059941e612d6c9bdbdc9ecd8f37646466f
Henry Lee <outshadow@sina.com>
2017-05-15 23:06:24 +0800

    add ejs

--- app/admin/controller/index.js
+++ app/admin/controller/index.js
@@ -1,6 +1,11 @@
 var indexController = function(){
-	this.indexAction = function(){
-		console.log('aaaaaaaaaaaaaaa');
+	this.indexAction = function(viewpath){
+		var pathname = viewpath+'index.html';
+		var content = FS.readFileSync(pathname, 'utf8');
+		var sys_name = '小馒头管理系统';
+		var result = EJS.render(content, {sys_name:sys_name});
+
+		return result;
 	};
 }
 
