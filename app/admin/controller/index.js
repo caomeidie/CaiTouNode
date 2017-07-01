aa97a3538614da9a4a8a20135e15a75500714c12
henry lee <outshadow@sina.com>
2017-07-01 17:13:21 +0800

    fixed template js

--- app/admin/controller/index.js
+++ app/admin/controller/index.js
@@ -1,17 +1,20 @@
 util = require('util');
 var indexController = function(){
-	this.loginAction = function(viewpath){
-		var filename = viewpath+'login.html';
+	this.loginAction = function(){
 		var sys_name = '小馒头管理系统';
 		temp.assign({sys_name:sys_name});
 		var result = temp.display();
 		return result;
 	};
 
-	this.testAction = function(viewpath){
+	this.testAction = function(){
 		var result = temp.display();
 		return result;
 	};
+
+	this.postloginAction = function(){
+		return POST;
+	}
 }
 
 module.exports = indexController;
