588635d1bbc16eb122b8fbb1207e3877100ef6a1
Henry Lee <outshadow_st@sina.com>
2017-07-05 18:47:54 +0800

    add

--- app/admin/controller/index.js
+++ app/admin/controller/index.js
@@ -1,4 +1,5 @@
 var upload = require(config.libPath+'upload');
+var gm = require('gm');  
 var indexController = function(){
 	this.loginAction = function(){
 		var sys_name = '小馒头管理系统';
@@ -14,8 +15,16 @@ var indexController = function(){
 
 	this.postloginAction = function(){
 		var file_path = './aaa.png';
-		upload.set_size(50);
-		upload.upload(POST.bbb,file_path);
+		var test = gm(file_path).resize('200', '200').stream(function(err, stdout, stderr) {
+                stdout.pipe(FS.createWriteStream("./ccc.png"));
+            });
+		// imageMagick(file_path).resize(150, 150, '!').autoOrient().write('./bbb.png', function(err){  
+  //           if (err) {  
+  //               console.log(err);
+  //           }
+  //       });
+		// upload.set_size(50);
+		// upload.upload(POST.bbb,file_path);
 		return '';
 	}
 }
