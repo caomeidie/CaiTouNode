62cdab0cdd20af4c53c88e3d4071ee63dd430ee0
Henry Lee <outshadow_st@sina.com>
2017-08-11 16:50:16 +0800

    fixed session done

--- app/admin/controller/index.js
+++ app/admin/controller/index.js
@@ -11,17 +11,23 @@ var indexController = function(web, temp){
 		// web.setCookie('name','xiaomimi',opt);
 		// web.setCookie('name2','xiaomimi2',opt);
 		// web.setSession('name2','xiaomimi2',opt);
-		// var arr = new Array();
-		// arr['name4'] = 'xiaomimi4';
-		// arr['name5'] = 'xiaomimi5';
-		// arr['name6'] = 'xiaomimi6';
+		var arr = new Array();
+		arr['name4'] = 'xiaomimi4';
+		arr['name5'] = 'xiaomimi5';
+		arr['name6'] = 'xiaomimi6';
 		// web.setCookie(arr);
 		// web.unsetCookie();
 		//console.log(web.getCookie());
 		//
 		//console.log(web.getCookie('name4'));
-		console.log(web.setSession('name', 'xiaomi'));
+		// web.setSession('name', 'xiaomi')
+		web.setSession(arr);
+		// web.setSession('name', {sys_name:sys_name});
 		//web.setSession('name', 'xiaomi');
+		console.log(web.getSession());
+		web.unsetSession('name5');
+		console.log(web.getSession());
+		web.unsetSession();
 		temp.assign({sys_name:sys_name});
 		var result = temp.display();
 		return result;
